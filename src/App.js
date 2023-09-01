6c8db788bcb144b194321070fa9e3f96ad794a2e
Nikolas Graham <nikolastgraham@gmail.com>
2023-09-01 14:48:55 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,20 +18,6 @@ const App = () => {
               different features such as:<br /> games, Wikipedia search, facial feature recognition, weather and more.
           </p>
         <hr />
-        <h2>ThankU Moments : <a href="https://dev.d2zibspy7fyk5y.amplifyapp.com/">link</a></h2>
-          <p>
-              This application serves as a modern integration of the pain of writing, stamping, and sending thank
-              yous for any occasion.
-          </p>
-          <p>
-              Made to make life easier for weddings, large gatherings, or even just a couple friends.
-          </p>
-          <p>
-              With a very intuitive design and easy UI, it makes it simple for users to send their thank you's in the
-              modern world.
-          </p>
-          <hr />
-      </div>
   );
 };
 
