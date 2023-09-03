3afe52ba89af50842d651946da23a9d5ab593218
Nikolas Graham <lichtschwert@live.com>
2023-09-02 23:11:45 -0700

    Updates

---
+++ precache-manifest.0bff0c58eabfeddf393111edb658439a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67514b381e97056c897faf0023ffb4c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f9e307ff183ed4bb6dc",
+    "url": "/static/css/main.f11fac86.chunk.css"
+  },
+  {
+    "revision": "b5c50630b00aa11b0486",
+    "url": "/static/js/2.83aa3c9a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.83aa3c9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f9e307ff183ed4bb6dc",
+    "url": "/static/js/main.9dc9ad31.chunk.js"
+  },
+  {
+    "revision": "ef18d2947006a88f5e82",
+    "url": "/static/js/runtime-main.a827d470.js"
+  }
+]);
